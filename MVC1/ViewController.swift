d9061978de94c6cff43f776210e4188e76f364bf
varolaksoy <pixtur@icloud.com>
2019-09-07 23:56:25 +0300

    Initial Commit

---
+++ MVC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC1
+//
+//  Created by Mac on 7.09.2019.
+//  Copyright © 2019 Varol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
